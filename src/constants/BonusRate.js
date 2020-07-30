332adf790f578f542e6ac35d393a0bcfd7fd7aee
YuukiHase <42062856+YuukiHase@users.noreply.github.com>
2020-07-30 21:32:19 +0700

    add constants

---
+++ src/constants/BonusRate.js
@@ -0,0 +1,22 @@
+export const bonusRates = [
+    {
+        value: 1,
+        label: '1',
+    },
+    {
+        value: 1.5,
+        label: '1.5',
+    },
+    {
+        value: 2,
+        label: '2',
+    },
+    {
+        value: 2.5,
+        label: '2.5',
+    },
+    {
+        value: 3,
+        label: '3',
+    },
+];
